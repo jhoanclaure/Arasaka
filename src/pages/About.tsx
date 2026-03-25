58af99405ce7a045d0fd714df03860fd5ded46b8
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2026-03-25 18:06:19 -0400

    Refactor: Color parrafos

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,6 @@ import { ImageUploader } from "../components/ui/ImageUploader";
 export default function About() {
   return (
     <section id="center">
-      <h1 >Acerca de nosotros</h1>
-      <p>Esta es una página adicional para probar React Router.</p>
       <ImageUploader></ImageUploader>
 
     </section>
