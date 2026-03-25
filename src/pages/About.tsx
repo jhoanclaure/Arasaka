14d0c593c9f35347e27d32b648a333339dd8df59
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2026-03-25 17:42:56 -0400

    Refactor: Color desing

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,9 +3,10 @@ import { ImageUploader } from "../components/ui/ImageUploader";
 export default function About() {
   return (
     <section id="center">
-      <h1>Acerca de nosotros</h1>
+      <h1 >Acerca de nosotros</h1>
       <p>Esta es una página adicional para probar React Router.</p>
       <ImageUploader></ImageUploader>
+
     </section>
   )
 }
